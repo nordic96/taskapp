6d98d13c16af84417f9ac5f09a94c7fd103a75ff
Gi Hun Ko <gihunko@Stephen-Kos-MacBook-Air.local>
2022-07-16 14:52:12 +0800

    updated project skeleton structure

---
+++ src/main/java/nordic/TaskApplication.java
@@ -0,0 +1,29 @@
+package nordic;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TaskApplication extends Application<TaskApplicationConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new TaskApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "TaskApplication";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<TaskApplicationConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final TaskApplicationConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
