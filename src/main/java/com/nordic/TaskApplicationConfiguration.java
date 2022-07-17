57efa2ce7122cc7473efbb1647818acc78f9ea7f
Gi Hun Ko <gihunko@Stephen-Kos-MacBook-Air.local>
2022-07-17 10:32:13 +0800

    configured swagger in app

--- src/main/java/com/nordic/TaskApplicationConfiguration.java
+++ src/main/java/com/nordic/TaskApplicationConfiguration.java
@@ -3,11 +3,14 @@ package com.nordic;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nordic.db.configuration.MongoDBConnection;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 
 import javax.validation.constraints.*;
 
 public class TaskApplicationConfiguration extends Configuration {
     private MongoDBConnection mongoDBConnection;
+    @JsonProperty("swagger")
+    private SwaggerBundleConfiguration swaggerBundleConfiguration;
 
 
     public MongoDBConnection getMongoDBConnection() {
@@ -16,5 +19,9 @@ public class TaskApplicationConfiguration extends Configuration {
 
     public void setMongoDBConnection(final MongoDBConnection mongoDBConnection) {
         this.mongoDBConnection = mongoDBConnection;
+    }
+
+    public SwaggerBundleConfiguration getSwaggerBundleConfiguration() {
+        return this.swaggerBundleConfiguration;
     }
 }
